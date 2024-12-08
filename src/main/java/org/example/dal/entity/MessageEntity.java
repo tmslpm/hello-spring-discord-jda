92fbbf87828947e1ae4fc34b97d0b82d9b5ae293
tmslpm <thomaslepomme@gmail.com>
2024-12-08 21:28:27 +0100

    added test example
    - add h2 database dependency to gradle script and version catalogs
    - add adds a simple test "MainTest"

--- src/main/java/org/example/dal/entity/MessageEntity.java
+++ src/main/java/org/example/dal/entity/MessageEntity.java
@@ -6,12 +6,14 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity(name = "message")
 @Table(name = "messages")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class MessageEntity extends BaseEntity {
 
   @Column(name = "content", nullable = false)
